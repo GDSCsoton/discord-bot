406223fd95760e05ff8695f08032e2f03a60929a
Britannio <britanniojarrett@gmail.com>
2021-09-21 16:18:24 +0100

    use env.properties to store the guild id.

--- src/main/java/club/sotondsc/bot/Main.java
+++ src/main/java/club/sotondsc/bot/Main.java
@@ -19,8 +19,12 @@ public class Main extends ListenerAdapter {
   public static void main(String[] args) throws LoginException, IOException {
     CommandClientBuilder commandBuilder = new CommandClientBuilder();
     var badgeCommand = new BadgeCommand();
-    commandBuilder.setPrefix("!").setOwnerId(Users.britannio).setCoOwnerIds(Users.britannio, Users.yaseen)
-        .forceGuildOnly("888015929873694741").addCommands(badgeCommand);
+    commandBuilder //
+        .setPrefix("!") //
+        .setOwnerId(Users.britannio) //
+        .setCoOwnerIds(Users.britannio, Users.yaseen) //
+        .forceGuildOnly(getGuildId()) //
+        .addCommands(badgeCommand);
     CommandClient commandClient = commandBuilder.build();
     commandClient.forcedGuildId();
 
@@ -29,24 +33,22 @@ public class Main extends ListenerAdapter {
 
     JDA jda = builder.addEventListeners(main, commandClient).setActivity(Activity.playing("Null Pointer Exception"))
         .build();
-    main.jda = jda;
     badgeCommand.jda = jda;
   }
 
-  JDA jda;
+  private static Properties getProperties() throws IOException {
+    InputStream input = Main.class.getClassLoader().getResourceAsStream("env.properties");
+    Properties prop = new Properties();
+    prop.load(input);
+    return prop;
+  }
 
   private static String getToken() throws IOException {
-    String token = System.getenv("DISCORD_TOKEN");
-    if (token != null)
-      return token;
-    InputStream input = Main.class.getClassLoader().getResourceAsStream("env.properties");
+    return getProperties().getProperty("discord.token");
+  }
 
-    Properties prop = new Properties();
-
-    // load a properties file from class path, inside static method
-    prop.load(input);
-
-    return prop.getProperty("discord.token");
+  private static String getGuildId() throws IOException {
+    return getProperties().getProperty("discord.guild");
   }
 
 }
